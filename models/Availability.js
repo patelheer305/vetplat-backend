b266dd6efba86b14c02168a17940e1ccdcceac6e
patelheer305 <patelheer305@gmail.com>
2025-08-29 11:44:07 +0530

    Delete models directory

--- models/Availability.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const availabilitySchema = new mongoose.Schema({
-  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  date: { type: String, required: true },
-  time: { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model("Availability", availabilitySchema);
